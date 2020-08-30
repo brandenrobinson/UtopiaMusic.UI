894bd9fbca37d41cb4821ac808268b0379903d4f
get0ut0fmyway <bresonate@gmail.com>
2020-08-30 09:57:36 -0400

    implementing blazorise

--- Utopia.UI.BlazorApp/Program.cs
+++ Utopia.UI.BlazorApp/Program.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 namespace Utopia.UI.BlazorApp
 {
@@ -15,11 +14,26 @@ namespace Utopia.UI.BlazorApp
 		public static async Task Main(string[] args)
 		{
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+			builder.Services
+				.AddBlazorise(options =>
+				{
+					options.ChangeTextOnKeyPress = true;
+				})
+				.AddBootstrapProviders()
+				.AddFontAwesomeIcons();
+
 			builder.RootComponents.Add<App>("app");
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-			await builder.Build().RunAsync();
+			var host = builder.Build();
+
+			host.Services
+			.UseBootstrapProviders()
+			.UseFontAwesomeIcons();
+
+			await host.RunAsync();
 		}
 	}
 }
