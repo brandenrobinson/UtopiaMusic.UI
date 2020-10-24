8c26ae145b93efccbd41d45ca5103a397f197b4e
get0ut0fmyway <bresonate@gmail.com>
2020-10-24 18:57:20 -0400

    reset

--- Utopia.UI.BlazorApp/Program.cs
+++ Utopia.UI.BlazorApp/Program.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Net.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
+using Microsoft.Extensions.Logging;
 
 namespace Utopia.UI.BlazorApp
 {
@@ -14,26 +15,11 @@ namespace Utopia.UI.BlazorApp
 		public static async Task Main(string[] args)
 		{
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-			builder.Services
-				.AddBlazorise(options =>
-				{
-					options.ChangeTextOnKeyPress = true;
-				})
-				.AddBootstrapProviders()
-				.AddFontAwesomeIcons();
-
 			builder.RootComponents.Add<App>("app");
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-			var host = builder.Build();
-
-			host.Services
-			.UseBootstrapProviders()
-			.UseFontAwesomeIcons();
-
-			await host.RunAsync();
+			await builder.Build().RunAsync();
 		}
 	}
 }
