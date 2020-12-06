4b1cfd3196da4e7b19d69e2bd826c53a1bf2d642
get0ut0fmyway <bresonate@gmail.com>
2020-12-06 14:14:06 -0500

    made a list

---
+++ UtopiaMusic.Shared/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UtopiaMusic.Shared
+{
+    public class Track
+    {
+        public int TrackId { get; set; }
+
+        public string Name { get; set; }
+
+        public int AlbumId { get; set; }
+    }
+}
