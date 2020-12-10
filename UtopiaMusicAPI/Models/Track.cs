c0c9341f321369881db0142bda924df77eeaf9da
get0ut0fmyway <bresonate@gmail.com>
2020-12-10 00:02:52 -0500

    working on db design

---
+++ UtopiaMusicAPI/Models/Track.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UtopiaMusicAPI.Models
+{
+    public class Track
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int? InitialAlbumId { get; set; }
+
+        public bool IsOnYouTube { get; set; }
+
+        public bool IsOnSoundcloud { get; set; }
+
+        public bool CanBePurchased { get; set; }
+
+        public string TrackName { get; set; }
+
+        public ICollection<Artist> Artists { get; set; }
+
+        public ICollection<Album> Albums { get; set; }
+    }
+
+    public class Artist
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public ICollection<Track> Tracks { get; set; }
+
+        public ICollection<Album> Albums { get; set; }
+    }
+
+    public class Album
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public ICollection<Track> Tracks { get; set; }
+
+        public ICollection<Artist> Artists { get; set; }
+    }
+}
---
+++ UtopiaMusicAPI/Models/UtopiaMusicContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UtopiaMusicAPI.Models
+{
+    public class UtopiaMusicContext : DbContext
+    {
+        public UtopiaMusicContext(DbContextOptions<UtopiaMusicContext> options) : base(options)
+        {
+
+        }
+    }
+}
